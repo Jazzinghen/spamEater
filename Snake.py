0d1c2c3e41fc38a3ecdf22ea0fe2d5e5769b00b7
Michele Bianchi <jazzinghen@gmail.com>
2010-05-19 18:48:50 +0200

    First working release :D Simply this connects to a NXT brick then logs all the data sent. Only the first two values are logged (An Unsigned INT, the sys counter, and an INT, the tacho counter for port B motor.). More will be released in a while.

--- Snake.py
+++ Snake.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 import lightblue
+import struct
+
+spam = lightblue.socket()
 
 datalog = open ("spam.log", "wt")
 
-datalog.write ("Fuffafuffa")
+print "Please select the NXT brick"
+
+(addr, port, name) = lightblue.selectservice()
+
+spam.connect((addr, port))
+
+print "Logging data..."
+
+while (1):
+  try: 
+    spam_buffer = spam.recv(64)
+    spam_buffer = str(struct.unpack('Ii56b', spam_buffer)[:2])
+    datalog.write (spam_buffer)
+    datalog.write ("\n" )
+  except Exception, msg:
+    print 'cazo, merda, cul ', str(msg)
+    break
+    
+spam.close()
