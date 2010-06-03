17a411a4c3c2f08bbd1c943049e5164f3aeae944
Michele Bianchi <jazzinghen@gmail.com>
2010-06-03 14:27:59 +0200

    Fixed recieved buffer size problem. There still are some bugs, so I'll need to kill all of them, but this version works better than the older ;)

--- Snake.py
+++ Snake.py
@@ -14,14 +14,21 @@ spam.connect((addr, port))
 
 print "Logging data..."
 
+lenbuf = lambda b : sum(map(len, b))
+
 while (1):
-  try: 
-    spam_buffer = spam.recv(64)
-    spam_buffer = str(struct.unpack('Ii56b', spam_buffer)[:2])
+  spam_buffer = []
+  try:
+    spam_buffer.append(spam.recv(64))   # Always read 64 bytes at a time!
+    while (lenbuf(spam_buffer) < 64) :
+      spam_buffer.extend(spam.recv(64 - lenbuf(spam_buffer)))
+    assert (lenbuf(spam_buffer) == 64)
+    print 'Packet recieved has a lenght of: ', lenbuf(spam_buffer)
+    spam_buffer = str(struct.unpack('2i3f2i3f6i', ''.join(spam_buffer))[:10])
     datalog.write (spam_buffer)
-    datalog.write ("\n" )
+    datalog.write ("\n")
   except Exception, msg:
-    print 'cazo, merda, cul ', str(msg)
+    print 'Due to something the data stream ended: ' , str(msg), lenbuf(spam_buffer)
     break
     
 spam.close()
